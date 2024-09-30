8b39e03d32199285b615b132f616db248b4fece8
llschall <llschall@gmx.com>
2024-09-30 23:10:03 +0200

    [Build] Update to Java 17

--- lib/src/test/kotlin/BuildTest.kt
+++ lib/src/test/kotlin/BuildTest.kt
@@ -8,15 +8,16 @@ import org.llschall.ardwloop.ArdwloopStarter
  */
 class BuildTest {
     @Test
-    fun checkVersions() {
+    fun checkArdwloop() {
         assertEquals("0.1.6", ArdwloopStarter.VERSION)
+        assertEquals(1003, ArdwloopStarter.VERSION_INT)
         assertEquals("0.1.3", ArdwloopExtStarter().ARDWLOOP_EXT_VERSION)
+        assertEquals(1001, ArdwloopExtStarter().ARDWLOOP_EXT_VERSION_INT)
     }
 
     @Test
-    fun checkSnapshot() {
-        assertEquals(1003, ArdwloopStarter.VERSION_INT)
-        assertEquals(1001, ArdwloopExtStarter().ARDWLOOP_EXT_VERSION_INT)
+    fun checkJava() {
+        assertEquals("17", System.getProperty("java.version").split(".")[0])
     }
 
     @Test
