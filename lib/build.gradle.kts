377b402a04ee155d574f0c10c4b5f1ea0787bb64
llschall <llschall@gmx.com>
2024-10-01 12:54:19 +0200

    [Build] Move publishing section

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.android.library)
+
+    `maven-publish`
+    signing
 }
 
 android {
@@ -67,4 +70,65 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+if (!project.hasProperty("token_usr")) {
+    println("=== Publish skipped because no credential is provided. ===")
+} else {
+    val user: String = project.properties["token_usr"].toString()
+    val pwd: String = project.properties["token_pwd"].toString()
+
+    afterEvaluate {
+        publishing {
+            publications {
+                create<MavenPublication>("ardwloop-ext") {
+                    from(components["release"])
+
+                    groupId = "io.github.llschall"
+                    artifactId = "ardwloop-ext"
+                    version = "0.1.3-SNAPSHOT"
+
+                    pom {
+                        signing {
+                            sign("publishing.publications.ardwloop-ext")
+                            sign("configurations.archives")
+                        }
+                        name = "ardwloop-ext"
+                        description =
+                            "An extended version of ardwloop, with Bluetooth support for Android."
+                        url = "https://github.com/llschall/ardwloop-ext"
+                        licenses {
+                            license {
+                                name = "The Apache License, Version 2.0"
+                                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                            }
+                        }
+                        developers {
+                            developer {
+                                id = "llschall"
+                                name = "Laurent Schall"
+                                email = "llschall@gmx.com"
+                            }
+                        }
+                        scm {
+                            url = "https://github.com/llschall/ardwloop-ext.git"
+                        }
+                    }
+                }
+            }
+            repositories {
+                maven {
+                    credentials {
+                        username = user
+                        password = pwd
+                    }
+                    url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+                    // url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+                }
+            }
+            signing {
+                sign(publishing.publications["ardwloop-ext"])
+            }
+        }
+    }
 }
