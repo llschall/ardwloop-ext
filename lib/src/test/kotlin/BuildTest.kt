15abf65180526475d0d8a5a54f55131cc7d028eb
llschall <llschall@gmx.com>
2024-12-29 18:53:21 +0100

    [Build] Bump ardwloop to 0.3.0

--- lib/src/test/kotlin/BuildTest.kt
+++ lib/src/test/kotlin/BuildTest.kt
@@ -10,7 +10,7 @@ import org.llschall.ardwloop.ext.ArdwloopExtStarter
 class BuildTest {
     @Test
     fun checkArdwloop() {
-        assertEquals("0.2.9", ArdwloopStarter.VERSION)
+        assertEquals("0.3.0", ArdwloopStarter.VERSION)
         assertEquals(1001, ArdwloopStarter.VERSION_INT)
         assertEquals("0.2.0", ArdwloopExtStarter().VERSION)
         assertEquals(1001, ArdwloopExtStarter().VERSION_INT)
