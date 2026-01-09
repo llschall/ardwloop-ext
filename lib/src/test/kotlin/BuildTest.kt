7d1fe9e00367db2c105b6b77e9aff361557f3fe8
llschall <llschall@gmx.com>
2026-01-09 17:03:53 +0100

    [Build] Adjust test

--- lib/src/test/kotlin/BuildTest.kt
+++ lib/src/test/kotlin/BuildTest.kt
@@ -10,7 +10,7 @@ import org.llschall.ardwloop.ext.ArdwloopExtStarter
 class BuildTest {
     @Test
     fun checkArdwloop() {
-        assertEquals("0.3.1", ArdwloopStarter.VERSION)
+        assertEquals("0.3.9", ArdwloopStarter.VERSION)
         assertEquals(1001, ArdwloopStarter.VERSION_INT)
         assertEquals("0.2.2", ArdwloopExtStarter().VERSION)
         assertEquals(1001, ArdwloopExtStarter().VERSION_INT)
